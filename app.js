7c9891e98f932474b3ecb7478e8c265896af5843
Taha Shieenavaz <tahashieenavaz@gmail.com>
2022-05-11 00:58:52 +0430

    Comments are added

--- app.js
+++ app.js
@@ -9,22 +9,83 @@
 const $ = document.querySelector.bind(document);
 const $$ = document.querySelectorAll.bind(document);
 
-function type(variable, type = "string") {
-  if (type == typeof variable) {
+function addClass(target, classToAdd) {
+  if (!type(target, 'element', true)) {
+    target = $(target);
+  }
+
+  if (target.classList.contains(classToAdd)) {
+    return false;
+  }
+
+  target.classList.add(classToAdd);
+
+  return true;
+}
+function removeClass(target, classToRemove) {
+  if (!type(target, 'element', true)) {
+    target = $(target);
+  }
+
+  if (!target.classList.contains(classToRemove)) {
+    return false;
+  }
+
+  target.classList.remove(classToRemove);
+
+  return true;
+}
+/**
+ * a utility functions which helps me to implicitly check the type for
+ * javascript variables and do not load Typescript for some minor checks.
+ * @param {*} variable
+ * @param {*} type
+ * @returns {boolean}
+ */
+function type(variable, providedType = 'string', bool = false) {
+  const panic = () => {
+    if (bool) return false;
+
+    const errorMessage =
+      "Type of variable doesn't match corresponding criteria!";
+    throw new Error(errorMessage);
+  };
+
+  if (providedType == 'element') {
+    if (variable instanceof Element || variable instanceof HTMLElement) {
+      return true;
+    } else return panic();
+  }
+
+  if (providedType == typeof variable) {
     return true;
   }
 
-  throw new Error("Type of variable doesn't match corresponding criteria!");
+  return panic();
+}
+/**
+ * Used to hide a page via its specific id in DOM.
+ * @param {string} target
+ * @returns {boolean} Returns false if page hiding is not successful, and exact
+ * opposite for the vice versa.
+ */
+function hidePage(target) {
+  if (removeClass(`#${target}.active`, 'active')) {
+    return true;
+  }
   return false;
 }
-function hidePage(target) {
-  $(`#${target}`).classList.remove("active");
-  return true;
-}
 
+/**
+ * Used to show a page via its specific id in DOM.
+ * @param {string} target
+ * @returns {boolean} Returns false if page showing is not successful, and exact
+ * opposite for the vice versa.
+ */
 function showPage(target) {}
-function gotoPage(target) {
-  type(target, "string");
 
-  $("section.active");
+function gotoPage(target) {
+  type(target, 'string');
+
+  $('section.active');
 }
