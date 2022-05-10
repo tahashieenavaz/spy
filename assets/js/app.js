361225b38883d074b82788d95e06582334a27bcb
Taha Shieenavaz <tahashieenavaz@gmail.com>
2022-05-11 03:20:05 +0430

    refactored app.js to an IFFE

--- assets/js/app.js
+++ assets/js/app.js
@@ -1,12 +1,14 @@
-if ('serviceWorker' in navigator) {
-  window.addEventListener('load', function () {
-    navigator.serviceWorker
-      .register('/assets/js/serviceWorker.js')
-      .then((registration) => registration.update())
-      .catch((err) => console.log('service worker not registered', err));
-  });
-}
-const $ = document.querySelector.bind(document);
-const $$ = document.querySelectorAll.bind(document);
-const { log } = console;
-const info = log;
+(function () {
+  if ('serviceWorker' in navigator) {
+    window.addEventListener('load', function () {
+      navigator.serviceWorker
+        .register('/assets/js/serviceWorker.js')
+        .then((registration) => registration.update())
+        .catch((err) => console.log('service worker not registered', err));
+    });
+  }
+  const $ = document.querySelector.bind(document);
+  const $$ = document.querySelectorAll.bind(document);
+  const { log } = console;
+  const info = log;
+})();
